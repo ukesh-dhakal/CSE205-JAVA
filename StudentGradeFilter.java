4e4736c0de0ae211f20ae173c982e76fd1893282
Ukesh Dhakal <130669170+ukesh-dhakal@users.noreply.github.com>
2026-06-18 07:02:57 +0545

    Update StudentGradeFilter.java

--- StudentGradeFilter.java
+++ StudentGradeFilter.java
@@ -1,13 +1,9 @@
 // Practice 2: Student Grade Filter
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class StudentGradeFilter {
     public static void main(String[] args) {
-        ArrayList<Integer> scores = new ArrayList<>(Arrays.asList(
-                95, 82, 39, 40, 76, 90, 12, 67, 88, 100
-        ));
+
+        int[] scores = {95, 82, 39, 40, 76, 90, 12, 67, 88, 100};
 
         for (int score : scores) {
             if (score >= 90) {
